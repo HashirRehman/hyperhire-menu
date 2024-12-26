77d5361dceb9983aa5536128cf98fde49215f4a0
Hashir Rehman <meethashirrehman@gmail.com>
2024-12-26 13:57:21 +0500

    Minor config and initial test page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,24 @@
+'use client'
+
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return (
-    <h1 className="border rounded text-red-900">HyperHire Menu</h1>
-  );
+  const [apiResponse, setApiResponse] = useState<string>("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`);
+        const text = await response.text();
+        setApiResponse(text);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setApiResponse('Error loading data');
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return <h1 className="border rounded text-red-900">{apiResponse}</h1>;
 }
