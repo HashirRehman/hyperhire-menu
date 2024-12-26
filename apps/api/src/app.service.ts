3d7689290efe0642dc1aafe076a20d55c1ebf8d4
Hashir Rehman <meethashirrehman@gmail.com>
2024-12-26 17:46:10 +0500

    Add test endpoint

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { PrismaService } from 'prisma/primsa.service';
 
 @Injectable()
 export class AppService {
+
+  constructor(private readonly prisma: PrismaService) {}
+
+  private logger = new Logger('User service');
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getAllUsers() {
+    const users = await this.prisma.test.findMany();
+    return  users;
   }
 }
